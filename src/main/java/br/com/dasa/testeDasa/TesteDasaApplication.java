6d0a69bfd46815faef15bd3968b090e2d95a2a4e
Alex Sandro de Almeida Souza <aalex_sandro@hotmail.com>
2021-07-10 22:49:14 -0300

    correcao de importacao de classes

--- src/main/java/br/com/dasa/testeDasa/TesteDasaApplication.java
+++ src/main/java/br/com/dasa/testeDasa/TesteDasaApplication.java
@@ -8,7 +8,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
